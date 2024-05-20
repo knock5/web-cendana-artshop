4d30f3032c74dfeb9593912d275169755442f01f
khoirulhuda07 <muhammadsirinmus123@gmai.com>
2024-05-20 23:50:52 +0700

    update

--- resources/views/user/cart.blade.php
+++ resources/views/user/cart.blade.php
@@ -5,7 +5,7 @@
 @section('body', 'sub_page')
 
 @section('content')
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 <style>
     table{
         border-radius: 10px 10px 10px 10px;
@@ -56,7 +56,7 @@
                                                     @endif</td>
                         <td id="p">{{$kr->Product->NAMA_PRODUK}}</td>
                         <td id="p" class="price1 ">{{$kr->Product->HARGA_PRODUK}}</td>
-                        <td id="p" ><input class="count1 p" type="number" id="jumlah_barang_{{$kr->ITEM_ID}}" value="{{$kr->JUMLAH}}" onchange="updateJumlah({{$kr->ITEM_ID}})" ></td>
+                        <td id="p" ><input class="count1 p" type="number" id="jumlah_barang_{{$kr->ITEM_ID}}" min="1" value="{{$kr->JUMLAH}}" onchange="updateJumlah({{$kr->ITEM_ID}})" ></td>
                     </tr>
                     @endforeach
                     <tr>
@@ -97,9 +97,12 @@ Pesan Sekarang
               <div class="rounded h-100 p-4">
                 <!-- input pertama -->
                 <div>
-                <input type="hidden" name="total" id="totalHarga3">
-                </div>
                 
+                </div>
+                <div class="form-floating mb-3">
+  <input type="text" class="form-control @error('alamat') is-invalid @enderror" name="total" id="totalHarga3" placeholder="name@example.com">
+  <label for="floatingInput">Total Harga</label>
+</div>
                 <div class="form-floating mb-3">
   <input type="text" class="form-control @error('alamat') is-invalid @enderror" name="alamat" id="floatingInput" placeholder="name@example.com">
   <label for="floatingInput">Alamat tujuan</label>
